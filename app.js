f243d0bff29af8b370d461640ee2c1c0efc9f314
Estuardo José Estrada <estuardo.ej@alliedglobalbpo.com>
2020-10-07 23:22:17 -0600

    Change and make a route for first route so healthcheck doesnt die

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const routes = require('./src/router/routes')
 const app = express()
+const router = express.Router()
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -11,6 +12,10 @@ app.use(bodyParser.urlencoded({
 }))
 
 app.use('/api', routes)
+
+app.get('/', (req, res, next) => {
+  res.json('WELCOME TO THE SIMPLE CALCULATOR. TO WORK WITH IT JUST CALL THE METHOD YOU WISH (sum, substract, multiply, divide)')
+})
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
